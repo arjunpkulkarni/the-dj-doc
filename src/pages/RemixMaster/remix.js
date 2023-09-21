2bf0a6fbba67b85ff5eee07a3ed5bfe801f00469
arjunkulkarni <arjunpk2@illinois.edu>
2023-09-21 00:50:16 -0500

    remixmaster chill

--- src/pages/RemixMaster/remix.js
+++ src/pages/RemixMaster/remix.js
@@ -5,7 +5,10 @@ import WaveSurfer from "wavesurfer.js";
 const RemixMaster = () => {
   const [audioSrc, setAudioSrc] = useState(null);
   const [audioPlayer, setAudioPlayer] = useState(null);
+  const [startTime, setStartTime] = useState(0);
+  const [endTime, setEndTime] = useState(0);
   const fileInputRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
     if (audioSrc) {
@@ -13,7 +16,7 @@ const RemixMaster = () => {
         container: "#waveform",
         backend: "WebAudio",
         waveColor: "white",
-        progressColor: "blue",
+        progressColor: "light-blue",
       });
 
       wavesurfer.load(audioSrc);
@@ -27,14 +30,31 @@ const RemixMaster = () => {
     if (file) {
       const objectURL = URL.createObjectURL(file);
       setAudioSrc(objectURL);
+      setStartTime(0); // Reset start and end times when a new file is selected
+      setEndTime(0);
     }
   };
 
   const playAudio = () => {
-    if (audioPlayer) {
-      audioPlayer.playPause();
+  
+  if (audioPlayer) {
+    if (audioPlayer.isPlaying()) {
+      audioPlayer.pause(); // Pause the audio if it's playing
+      setIsPlaying(false);
+    } else {
+      // Calculate the start position in seconds
+      const startPosition = startTime;
+      
+      // Seek to the calculated start position
+      audioPlayer.seekTo(startPosition);
+      
+      audioPlayer.play(); // Start playing from the specified position
+
+      setIsPlaying(true);
     }
+  }
   };
+
 
   const openFileInput = () => {
     fileInputRef.current.click();
@@ -42,17 +62,40 @@ const RemixMaster = () => {
 
   return (
     <div className="remix-master-container">
-      <h1>Remix Master</h1>
-      <button onClick={openFileInput}>Choose File</button>
-      <input
-        type="file"
-        accept=".mp3,.wav"
-        onChange={handleFileInputChange}
-        ref={fileInputRef}
-        style={{ display: "none" }}
-      />
-      <button onClick={playAudio}>Play</button>
-      <div id="waveform"></div>
+      <div className="title_box">
+        <h1 className="title">Remix Master</h1>
+      </div>
+
+      <div className="button_open_container">
+        <button onClick={openFileInput} className="button_open">
+          Choose File
+        </button>
+        <input
+          type="file"
+          accept=".mp3,.wav"
+          onChange={handleFileInputChange}
+          ref={fileInputRef}
+          style={{ display: "none" }}
+        />
+      </div>
+
+      <div className="button_play_container">
+        <button onClick={playAudio} className="button_play">
+          {isPlaying ? "Pause" : "Play"}
+        </button>
+      </div>
+
+      <div className="editor_container">
+        <div className="toolbar">
+          <button className="toolbar-button">Loop</button>
+          <button className="toolbar-button">Crossfader</button>
+          <button className="toolbar-button">FX</button>
+          <button className="toolbar-button">Undo</button>
+          <button className="toolbar-button">Redo</button>
+      </div>
+
+        <div id="waveform"></div>
+      </div>
     </div>
   );
 };
